dc4285995d5386edce446963fa70b898506d71c6
Vitor Nunes <stecks10@gmail.com>
2023-02-24 08:56:04 -0300

    validando dados

--- js/script.js
+++ js/script.js
@@ -10,11 +10,23 @@ form.onsubmit = event => {
   const weight = inputWeight.value;
   const height = inputHeight.value;
 
+  const showAlertError = notNumber(weight) || notNumber(height)
+
+  if (showAlertError) {
+    console.log('Mostrar erro')
+    return;
+  }
+
+
   const result = IMC(weight, height);
   const message = `Seu IMC é ${result}`;
 
   Modal.message.innerText = message;
   Modal.open();
+}
+
+function notNumber(value) {
+  return isNaN(value) || value == '';
 }
 
 function IMC(weight, height) {

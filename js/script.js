4ecc4a6279512abb5dd94eaeafa2b1af43af689e
Vitor Nunes <stecks10@gmail.com>
2023-02-27 10:07:31 -0300

    modularizar as funcoes utilitarias

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 import { Modal } from './modal.js';
 import { AlertError } from './alert-error.js';
+import { notNumber, IMC } from './utils.js'
 
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
@@ -25,12 +26,4 @@ form.onsubmit = event => {
 
   Modal.message.innerText = message;
   Modal.open();
-}
-
-function notNumber(value) {
-  return isNaN(value) || value == '';
-}
-
-function IMC(weight, height) {
-  return (weight / ((height / 100) ** 2)).toFixed(2);
 }
